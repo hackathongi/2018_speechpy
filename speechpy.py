91fe23e808e252e2d5a43f0d1e53befea9da94a0
LemonBF <lolddhol@gmail.com>
2018-03-03 11:16:22 +0100

    Test

--- speechpy.py
+++ speechpy.py
@@ -6,11 +6,21 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def hello_world():
+    with open("api-key.json") as f:
+        GOOGLE_CLOUD_SPEECH_CREDENTIALS = f.read()
+
+    with sr.AudioFile("/static/Alcohol.mp3") as source:
+        audio = r.record(source)
+
+    text = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+    all_text.append(text)
+
+    print(text)
+
     r = sr.Recognizer()
+    files = os.listdir('parts/')
     audio = ''
     text = ''
-
-
 
     # try:
     #     text = r.recognize_google(audio)
